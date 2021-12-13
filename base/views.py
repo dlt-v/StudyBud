37600bd8dfff4177022e9e8fb356392ec42023a6
Tomek Michalski <tmm.michalski@gmail.com>
2021-12-13 20:00:39 +0100

    add database models

--- base/views.py
+++ base/views.py
@@ -2,33 +2,32 @@ from typing import Any, List
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from typing import List, Dict, Any
+from .models import Room
 # Create your views here.
 
-rooms: List[Dict[Any, Any]] = [
-    {
-        'id': 0,
-        'name': 'Let\'s learn python!'
-    },
-    {
-        'id': 1,
-        'name': 'Design with me'
-    },
-    {
-        'id': 2,
-        'name': 'Frontend developers'
-    }
-]
+# rooms: List[Dict[Any, Any]] = [
+#     {
+#         'id': 0,
+#         'name': 'Let\'s learn python!'
+#     },
+#     {
+#         'id': 1,
+#         'name': 'Design with me'
+#     },
+#     {
+#         'id': 2,
+#         'name': 'Frontend developers'
+#     }
+# ]
 
 
 def home(request: HttpRequest) -> HttpResponse:
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request: HttpRequest, pk: str) -> HttpResponse:
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
