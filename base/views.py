e421fabfc62ff02b271b77484b22a2e17b3702db
Tomek Michalski <tmm.michalski@gmail.com>
2021-12-13 19:04:08 +0100

    set up dynamic template views

--- base/views.py
+++ base/views.py
@@ -1,11 +1,34 @@
+from typing import Any, List
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from typing import List, Dict, Any
 # Create your views here.
+
+rooms: List[Dict[Any, Any]] = [
+    {
+        'id': 0,
+        'name': 'Let\'s learn python!'
+    },
+    {
+        'id': 1,
+        'name': 'Design with me'
+    },
+    {
+        'id': 2,
+        'name': 'Frontend developers'
+    }
+]
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request: HttpRequest) -> HttpResponse:
-    return render(request, 'room.html')
+def room(request: HttpRequest, pk: str) -> HttpResponse:
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
