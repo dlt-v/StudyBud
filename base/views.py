61f3485f0bbfeefa962ffc7176156682095ac6c4
Tomek Michalski <tmm.michalski@gmail.com>
2021-12-14 12:15:23 +0100

    add delete room page

--- base/views.py
+++ base/views.py
@@ -20,7 +20,7 @@ def room(request: HttpRequest, pk: str) -> HttpResponse:
 
 def createRoom(request: HttpRequest):
     form = RoomForm()
-    
+
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -29,3 +29,25 @@ def createRoom(request: HttpRequest):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request: HttpRequest, pk: str) -> HttpResponse:
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request: HttpRequest, pk: str) -> HttpResponse:
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
