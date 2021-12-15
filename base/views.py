e6d5b0685f17dff2636cabbc6b379eac69998c60
Tomek Michalski <tmm.michalski@gmail.com>
2021-12-15 16:43:00 +0100

    add user registration form

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Room, Topic
 
@@ -13,11 +14,12 @@ from .forms import RoomForm
 
 
 def loginPage(request: HttpResponse):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -33,8 +35,25 @@ def loginPage(request: HttpResponse):
         else:
             messages.error(request, 'Username or password does not exist. :(')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
+
+
+def registerUser(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Oops! Something went wrong!')
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def logoutUser(request):
