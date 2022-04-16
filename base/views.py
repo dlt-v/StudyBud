b92fafbbb4e36a38aeed089288f9040b6fe7b13e
Tomek Michalski <tmm.michalski@gmail.com>
2022-04-16 08:56:04 +0200

    setup dynamic urls in home.html

--- base/views.py
+++ base/views.py
@@ -26,5 +26,11 @@ def home(request):
 
 
 def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
     # return HttpResponse('You\'re in rooms!')
-    return render(request, 'base/room.html')
+    return render(request, 'base/room.html', context)
