64d9a2e805be504540153e6ca6189266f7214f27
Delta.v <tmm.michalski@gmail.com>
2022-04-21 12:09:39 +0200

    add user registration handling

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Room, Topic
@@ -12,12 +13,12 @@ from .forms import RoomForm
 
 
 def login_page(request: HttpRequest):
-
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -33,8 +34,26 @@ def login_page(request: HttpRequest):
         else:
             messages.error(request, 'Username or password does not exist.')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
+
+
+def register_user(request: HttpRequest):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(
+                request, 'An error has occured during registration.')
+
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def logout_user(request: HttpRequest):
