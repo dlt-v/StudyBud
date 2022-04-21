705f6e79accb2f71fa993f5d6b9a886d8d50fd2d
Delta.v <tmm.michalski@gmail.com>
2022-04-21 15:08:34 +0200

    add simple comment system

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 # Create your views here.
 
@@ -80,10 +80,24 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 
-def room(request, pk):
+def room(request: HttpRequest, pk):
     room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
 
-    context = {'room': room}
+    context = {
+        'room': room,
+        'room_messages': room_messages,
+        'participants': participants
+    }
     # return HttpResponse('You\'re in rooms!')
     return render(request, 'base/room.html', context)
 
@@ -131,3 +145,15 @@ def delete_room(request: HttpRequest, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'object': room})
+
+@login_required(login_url='login')
+def delete_message(request: HttpRequest, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not the owner of this room.')
+
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'object': message})
