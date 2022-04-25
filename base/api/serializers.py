da03b2ab71519312bc4463898ec9f7f5ab6890ac
Delta.v <tmm.michalski@gmail.com>
2022-04-25 18:38:29 +0200

    add cors for all units

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,6 +1,6 @@
-from pyexpat import model
 from rest_framework.serializers import ModelSerializer
 from base.models import Room
+
 
 class RoomSerializer(ModelSerializer):
     class Meta:
--- studybud/settings.py
+++ studybud/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'base.apps.BaseConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -130,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
